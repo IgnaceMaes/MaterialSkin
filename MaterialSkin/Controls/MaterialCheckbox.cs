05743d0be0828acffc0b7d0be29adf61297e79b9
Mavamaarten <mvg_da_best@hotmail.com>
2014-12-20 17:54:26 +0100

    Optimized imports

--- MaterialSkin/Controls/MaterialCheckbox.cs
+++ MaterialSkin/Controls/MaterialCheckbox.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MaterialSkin.Controls
