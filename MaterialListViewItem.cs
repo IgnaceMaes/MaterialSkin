31e75e69d57fe9435469a53370699be734b84fc9
DevinPower <DevinPower@users.noreply.github.com>
2016-02-04 14:43:22 -0500

    Create MaterialListViewItem.cs
    For filling the material list view item's backcolor as a progress bar

---
+++ MaterialListViewItem.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MaterialSkin.Controls
+{
+    public class MaterialListViewItem : ListViewItem
+    {
+        public float fillPercent = 0;
+        public Color highlightColor = Color.Red;
+    }
+}
