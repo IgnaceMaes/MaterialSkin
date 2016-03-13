6669a03b48e14b554dce7032ae58ed33426e4d63
tjachmann <tjachmann@gmx-topmail.de>
2016-03-13 15:30:00 +0100

    Overwrite to disable windows automatic form scaling

--- MaterialSkinExample/Program.cs
+++ MaterialSkinExample/Program.cs
@@ -5,18 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MaterialSkin;
+using System.Runtime.InteropServices;
 
 namespace MaterialSkinExample
 {
     static class Program
     {
+        [DllImport("Shcore.dll", SetLastError = true)]
+        static extern int SetProcessDpiAwareness(int i);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            SetProcessDpiAwareness(2);
+
             Application.EnableVisualStyles();
+            
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
