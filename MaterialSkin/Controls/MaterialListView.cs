04fd91027e1fc067a2614761d9dacb7db9c75671
Ignace Maes <contact@ignacemaes.com>
2016-08-04 14:06:50 +0200

    Fixed #113, Merged #75 manually

--- MaterialSkin/Controls/MaterialListView.cs
+++ MaterialSkin/Controls/MaterialListView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,8 +20,10 @@ namespace MaterialSkin.Controls
 		public MouseState MouseState { get; set; }
 		[Browsable(false)]
 		public Point MouseLocation { get; set; }
+        [Browsable(false)]
+        private ListViewItem HoveredItem { get; set; }
 
-		public MaterialListView()
+        public MaterialListView()
 		{
 			GridLines = false;
 			FullRowSelect = true;
@@ -43,15 +46,21 @@ namespace MaterialSkin.Controls
 			{
 				MouseState = MouseState.OUT; 
 				MouseLocation = new Point(-1, -1);
-				Invalidate();
+                HoveredItem = null;
+                Invalidate();
 			};
 			MouseDown += delegate { MouseState = MouseState.DOWN; };
 			MouseUp += delegate{ MouseState = MouseState.HOVER; };
 			MouseMove += delegate(object sender, MouseEventArgs args)
 			{
 				MouseLocation = args.Location;
-				Invalidate();
-			};
+                var currentHoveredItem = this.GetItemAt(MouseLocation.X, MouseLocation.Y);
+                if (HoveredItem != currentHoveredItem)
+                {
+                    HoveredItem = currentHoveredItem;
+                    Invalidate();
+                }
+            };
 		}
 
 		protected override void OnDrawColumnHeader(DrawListViewColumnHeaderEventArgs e)
@@ -93,11 +102,11 @@ namespace MaterialSkin.Controls
 			{
 				//Draw text
 				g.DrawString(subItem.Text, SkinManager.ROBOTO_MEDIUM_10, SkinManager.GetPrimaryTextBrush(),
-								 new Rectangle(subItem.Bounds.Location.X + ITEM_PADDING, ITEM_PADDING, subItem.Bounds.Width - 2 * ITEM_PADDING, subItem.Bounds.Height - 2 * ITEM_PADDING),
+								 new Rectangle(subItem.Bounds.X + ITEM_PADDING, ITEM_PADDING, subItem.Bounds.Width - 2 * ITEM_PADDING, subItem.Bounds.Height - 2 * ITEM_PADDING),
 								 getStringFormat());
 			}
 
-			e.Graphics.DrawImage((Image) b.Clone(), e.Item.Bounds.Location);
+			e.Graphics.DrawImage((Image) b.Clone(), new Point(0, e.Item.Bounds.Location.Y));
 			g.Dispose();
 			b.Dispose();
 		}
@@ -113,14 +122,46 @@ namespace MaterialSkin.Controls
 			};
 		}
 
-		protected override void OnCreateControl()
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class LogFont
+        {
+            public int lfHeight = 0;
+            public int lfWidth = 0;
+            public int lfEscapement = 0;
+            public int lfOrientation = 0;
+            public int lfWeight = 0;
+            public byte lfItalic = 0;
+            public byte lfUnderline = 0;
+            public byte lfStrikeOut = 0;
+            public byte lfCharSet = 0;
+            public byte lfOutPrecision = 0;
+            public byte lfClipPrecision = 0;
+            public byte lfQuality = 0;
+            public byte lfPitchAndFamily = 0;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string lfFaceName = string.Empty;
+        }
+
+        protected override void OnCreateControl()
 		{
 			base.OnCreateControl();
 
-			//This is a hax for the needed padding.
-			//Another way would be intercepting all ListViewItems and changing the sizes, but really, that will be a lot of work
-			//This will do for now.
-			Font = new Font(SkinManager.ROBOTO_MEDIUM_12.FontFamily, 24);
-		}
+            // This hack tries to apply the Roboto (24) font to all ListViewItems in this ListView
+            // It only succeeds if the font is installed on the system.
+            // Otherwise, a default sans serif font is used.
+            Font roboto24 = new Font(SkinManager.ROBOTO_MEDIUM_12.FontFamily, 24);
+            LogFont roboto24logfont = new LogFont();
+            roboto24.ToLogFont(roboto24logfont);
+
+            try
+            {
+                // Font.FromLogFont is the method used when drawing ListViewItems. I 'test' it in this safer context to avoid unhandled exceptions later.
+                Font = Font.FromLogFont(roboto24logfont);
+            }
+            catch (ArgumentException)
+            {
+                Font = new Font(FontFamily.GenericSansSerif, 24);
+            }
+        }
 	}
 }
--- MaterialSkinExample/MainForm.cs
+++ MaterialSkinExample/MainForm.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Reflection;
-using System.Threading;
 using System.Windows.Forms;
 using MaterialSkin;
 using MaterialSkin.Controls;
