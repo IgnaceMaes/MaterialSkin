278fd44535f207a42b330c75bd3ce876b3341b4f
Don Steele <coachsteele@nafocyberpatriot.com>
2018-12-23 16:23:22 -0500

    Added new multiline text box

---
+++ MaterialSkin/Controls/MaterialMultiLineTextField.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using MaterialSkin.Animations;
+
+namespace MaterialSkin.Controls
+{
+    public class MaterialMultiLineTextField : TextBox , IMaterialControl
+    {
+        //Properties for managing the material design properties
+        [Browsable(false)]
+        public int Depth { get; set; }
+        [Browsable(false)]
+        public MaterialSkinManager SkinManager => MaterialSkinManager.Instance;
+        [Browsable(false)]
+        public MouseState MouseState { get; set; }
+
+             
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, string lParam);
+
+            private const int EM_SETCUEBANNER = 0x1501;
+            private const char EmptyChar = (char)0;
+            private const char VisualStylePasswordChar = '\u25CF';
+            private const char NonVisualStylePasswordChar = '\u002A';
+
+            private string hint = string.Empty;
+            public string Hint
+            {
+                get { return hint; }
+                set
+                {
+                    hint = value;
+                    SendMessage(Handle, EM_SETCUEBANNER, (int)IntPtr.Zero, Hint);
+                }
+            }
+
+            private char _passwordChar = EmptyChar;
+            public new char PasswordChar
+            {
+                get { return _passwordChar; }
+                set
+                {
+                    _passwordChar = value;
+                    SetBasePasswordChar();
+                }
+            }
+
+            public new void SelectAll()
+            {
+                BeginInvoke((MethodInvoker)delegate ()
+                {
+                    base.Focus();
+                    base.SelectAll();
+                });
+            }
+
+            public new void Focus()
+            {
+                BeginInvoke((MethodInvoker)delegate ()
+                {
+                    base.Focus();
+                });
+            }
+
+            private char _useSystemPasswordChar = EmptyChar;
+            public new bool UseSystemPasswordChar
+            {
+                get { return _useSystemPasswordChar != EmptyChar; }
+                set
+                {
+                    if (value)
+                    {
+                        _useSystemPasswordChar = Application.RenderWithVisualStyles ? VisualStylePasswordChar : NonVisualStylePasswordChar;
+                    }
+                    else
+                    {
+                        _useSystemPasswordChar = EmptyChar;
+                    }
+
+                    SetBasePasswordChar();
+                }
+            }
+
+            private void SetBasePasswordChar()
+            {
+                base.PasswordChar = UseSystemPasswordChar ? _useSystemPasswordChar : _passwordChar;
+            }
+
+            public MaterialMultiLineTextField()
+            {
+
+            base.OnCreateControl();
+            
+                MaterialContextMenuStrip cms = new MaterialSingleLineTextField.TextBoxContextMenuStrip();
+                cms.Opening += ContextMenuStripOnOpening;
+                cms.OnItemClickStart += ContextMenuStripOnItemClickStart;
+                this.Multiline = true;                
+                
+                ContextMenuStrip = cms;
+            BorderStyle = BorderStyle.None;
+            Font = SkinManager.ROBOTO_REGULAR_11;
+            BackColor = SkinManager.GetApplicationBackgroundColor();
+            ForeColor = SkinManager.GetPrimaryTextColor();
+            BackColorChanged += (sender, args) => ForeColor = SkinManager.GetPrimaryTextColor();
+            ForeColorChanged  += (sender, args) => ForeColor = SkinManager.GetPrimaryTextColor();
+
+        }
+
+           
+
+            private void ContextMenuStripOnItemClickStart(object sender, ToolStripItemClickedEventArgs toolStripItemClickedEventArgs)
+            {
+                switch (toolStripItemClickedEventArgs.ClickedItem.Text)
+                {
+                    case "Undo":
+                        Undo();
+                        break;
+                    case "Cut":
+                        Cut();
+                        break;
+                    case "Copy":
+                        Copy();
+                        break;
+                    case "Paste":
+                        Paste();
+                        break;
+                    case "Delete":
+                        SelectedText = string.Empty;
+                        break;
+                    case "Select All":
+                        SelectAll();
+                        break;
+                }
+            }
+
+            private void ContextMenuStripOnOpening(object sender, CancelEventArgs cancelEventArgs)
+            {
+                var strip = sender as MaterialSingleLineTextField.TextBoxContextMenuStrip;
+                if (strip != null)
+                {
+                    strip.Undo.Enabled = CanUndo;
+                    strip.Cut.Enabled = !string.IsNullOrEmpty(SelectedText);
+                    strip.Copy.Enabled = !string.IsNullOrEmpty(SelectedText);
+                    strip.Paste.Enabled = Clipboard.ContainsText();
+                    strip.Delete.Enabled = !string.IsNullOrEmpty(SelectedText);
+                    strip.SelectAll.Enabled = !string.IsNullOrEmpty(Text);
+                }
+            }
+        }
+    }
+
