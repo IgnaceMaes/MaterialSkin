0c8492572a647a28d969082aac01aec8aeef64eb
leocb <leobottaro@gmail.com>
2020-01-24 19:07:32 -0300

    Fix Shadows behaviour when changing component visibility and when changing parents

--- MaterialSkin/Controls/MaterialFloatingActionButton.cs
+++ MaterialSkin/Controls/MaterialFloatingActionButton.cs
@@ -91,7 +91,36 @@
         protected override void OnParentChanged(EventArgs e)
         {
             base.OnParentChanged(e);
-            if (DrawShadows && Parent != null) Parent.Paint += drawShadowOnParent;
+            if (DrawShadows && Parent != null) AddShadowPaintEvent(Parent, drawShadowOnParent);
+            if (_oldParent != null) RemoveShadowPaintEvent(_oldParent, drawShadowOnParent);
+            _oldParent = Parent;
+        }
+        Control _oldParent;
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            base.OnVisibleChanged(e);
+            if (Parent == null) return;
+            if (Visible)
+                AddShadowPaintEvent(Parent, drawShadowOnParent);
+            else
+                RemoveShadowPaintEvent(Parent, drawShadowOnParent);
+        }
+
+        bool _shadowDrawEventSubscribed = false;
+        private void AddShadowPaintEvent(Control control, PaintEventHandler shadowPaintEvent)
+        {
+            if (_shadowDrawEventSubscribed) return;
+            control.Paint += shadowPaintEvent;
+            control.Invalidate();
+            _shadowDrawEventSubscribed = true;
+        }
+        private void RemoveShadowPaintEvent(Control control, PaintEventHandler shadowPaintEvent)
+        {
+            if (!_shadowDrawEventSubscribed) return;
+            control.Paint -= shadowPaintEvent;
+            control.Invalidate();
+            _shadowDrawEventSubscribed = false;
         }
 
         private void setSize(bool mini)
@@ -116,8 +145,7 @@
         {
             if (Parent == null)
             {
-                ((Control)sender).Paint -= drawShadowOnParent;
-                ((Control)sender).Invalidate();
+                RemoveShadowPaintEvent((Control)sender, drawShadowOnParent);
                 return;
             }
 
