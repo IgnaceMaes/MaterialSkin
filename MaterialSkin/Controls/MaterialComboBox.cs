2910949aaee1839b5fbd4a7f485e790fec2ce1e2
Gabriel Stefan B <gabigtr5@gmail.com>
2021-05-30 14:47:29 +0300

    Update MaterialComboBox.cs

--- MaterialSkin/Controls/MaterialComboBox.cs
+++ MaterialSkin/Controls/MaterialComboBox.cs
@@ -117,7 +117,7 @@
                 AnimationType = AnimationType.EaseInOut
             };
             _animationManager.OnAnimationProgress += sender => Invalidate();
-            _animationManager.OnAnimationFinished += sender => { _animationManager.SetProgress(0); };
+            _animationManager.OnAnimationFinished += sender => _animationManager.SetProgress(0);
             DropDownClosed += (sender, args) =>
             {
                 MouseState = MouseState.OUT;
