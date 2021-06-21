ccbac5df7119dd39ff2c6ddfc3fa8ec2dc752763
orapps44 <77468294+orapps44@users.noreply.github.com>
2021-06-21 21:12:44 +0200

    Update AssemblyInfo.cs

--- MaterialSkin/Properties/AssemblyInfo.cs
+++ MaterialSkin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MaterialSkin")]
-[assembly: AssemblyCopyright("Copyright Leonardo C Bottaro © 2020")]
+[assembly: AssemblyCopyright("Copyright Leonardo C Bottaro © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyFileVersion("2.2.1.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
