263edea30ab442b4dff40d901f547125523064c7
orapps44 <77468294+orapps44@users.noreply.github.com>
2021-09-13 21:59:58 +0200

    Update MaterialMultiLineTextBox2.cs

--- MaterialSkin/Controls/MaterialMultiLineTextBox2.cs
+++ MaterialSkin/Controls/MaterialMultiLineTextBox2.cs
@@ -53,20 +53,8 @@ using MaterialSkin.Animations;
             set
             {
                 _enabled = value;
-                base.Enabled = true;
-                if (_enabled!=true)
-                {
-                    baseTextBox.ForeColor = ColorHelper.RemoveAlpha(SkinManager.TextDisabledOrHintColor, BackColor);
-                    baseTextBox.ReadOnly = true;
-                    baseTextBox.TabStop = false;
-                    base.Parent.Focus();
-                }
-                else
-                {
-                    baseTextBox.ForeColor = ColorHelper.RemoveAlpha(SkinManager.TextHighEmphasisColor, BackColor);
-                    baseTextBox.ReadOnly = ReadOnly;
-                    baseTextBox.TabStop = true;
-                }
+                base.Enabled = _enabled;
+                baseTextBox.Enabled = _enabled;
                 this.Invalidate();
             }
         }
@@ -1099,6 +1087,8 @@ using MaterialSkin.Animations;
 
             g.Clear(Parent.BackColor);
             SolidBrush backBrush = new SolidBrush(DrawHelper.BlendColor(Parent.BackColor, SkinManager.BackgroundAlternativeColor, SkinManager.BackgroundAlternativeColor.A));
+
+            //backColor
             g.FillRectangle(
                 !_enabled ? SkinManager.BackgroundDisabledBrush : // Disabled
                 isFocused ? SkinManager.BackgroundFocusBrush :  // Focused
@@ -1183,171 +1173,5 @@ using MaterialSkin.Animations;
  
         }
 
-        protected class BaseTextBox : RichTextBox, IMaterialControl
-        {
-            //Properties for managing the material design properties
-            [Browsable(false)]
-            public int Depth { get; set; }
-
-            [Browsable(false)]
-            public MaterialSkinManager SkinManager => MaterialSkinManager.Instance;
-
-            [Browsable(false)]
-            public MouseState MouseState { get; set; }
-
-            private string hint = string.Empty;
-            public string Hint
-            {
-                get { return hint; }
-                set
-                {
-                    hint = value;
-                }
-            }
-
-            public new void SelectAll()
-            {
-                BeginInvoke((MethodInvoker)delegate ()
-                {
-                    base.Focus();
-                    base.SelectAll();
-                });
-            }
-
-            public BaseTextBox()
-            {
-                MaterialContextMenuStrip cms = new TextBoxContextMenuStrip();
-                cms.Opening += ContextMenuStripOnOpening;
-                cms.OnItemClickStart += ContextMenuStripOnItemClickStart;
-                ContextMenuStrip = cms;
-            }
-
-            protected override void OnGotFocus(EventArgs e)
-            {
-                base.OnGotFocus(e);
-                Invalidate();
-            }
-
-            protected override void OnLostFocus(EventArgs e)
-            {
-                base.OnLostFocus(e);
-                Invalidate();
-            }
-
-            private const int WM_ENABLE = 0x0A;
-            private const int WM_PAINT = 0xF;
-            private const UInt32 WM_USER = 0x0400;
-            private const UInt32 EM_SETBKGNDCOLOR = (WM_USER + 67);
-            private const UInt32 WM_KILLFOCUS = 0x0008;
-            protected override void WndProc(ref Message m)
-            {
-                base.WndProc(ref m);
-
-                if (m.Msg == WM_PAINT)
-                {
-                    if (m.Msg == WM_ENABLE)
-                    {
-                        Graphics g = Graphics.FromHwnd(Handle);
-                        Rectangle bounds = new Rectangle(0, 0, Width , Height );
-                        g.FillRectangle(SkinManager.BackgroundDisabledBrush, bounds);
-                    }
-                }
-
-                if (m.Msg == WM_PAINT && String.IsNullOrEmpty(Text))
-                {
-                    using (NativeTextRenderer NativeText = new NativeTextRenderer(Graphics.FromHwnd(m.HWnd)))
-                    {
-                        NativeText.DrawTransparentText(
-                        Hint,
-                        SkinManager.getFontByType(MaterialSkinManager.fontType.Subtitle1),
-                        !this.ReadOnly ?
-                        ColorHelper.RemoveAlpha(SkinManager.TextMediumEmphasisColor, BackColor) : // not focused
-                        ColorHelper.RemoveAlpha(SkinManager.TextDisabledOrHintColor, BackColor), // Disabled
-                        ClientRectangle.Location,
-                        ClientRectangle.Size,
-                        NativeTextRenderer.TextAlignFlags.Left | NativeTextRenderer.TextAlignFlags.Top);
-                    }
-                }
-
-                if (m.Msg == EM_SETBKGNDCOLOR)
-                {
-                    Invalidate();
-                }
-
-                if (m.Msg == WM_KILLFOCUS) //set border back to normal on lost focus
-                {
-                    Invalidate();
-                }
-
-            }
-
-
-            private void ContextMenuStripOnItemClickStart(object sender, ToolStripItemClickedEventArgs toolStripItemClickedEventArgs)
-            {
-                switch (toolStripItemClickedEventArgs.ClickedItem.Text)
-                {
-                    case "Undo":
-                        Undo();
-                        break;
-                    case "Cut":
-                        Cut();
-                        break;
-                    case "Copy":
-                        Copy();
-                        break;
-                    case "Paste":
-                        Paste();
-                        break;
-                    case "Delete":
-                        SelectedText = string.Empty;
-                        break;
-                    case "Select All":
-                        SelectAll();
-                        break;
-                }
-            }
-
-            private void ContextMenuStripOnOpening(object sender, CancelEventArgs cancelEventArgs)
-            {
-                var strip = sender as TextBoxContextMenuStrip;
-                if (strip != null)
-                {
-                    strip.undo.Enabled = CanUndo;
-                    strip.cut.Enabled = !string.IsNullOrEmpty(SelectedText);
-                    strip.copy.Enabled = !string.IsNullOrEmpty(SelectedText);
-                    strip.paste.Enabled = Clipboard.ContainsText();
-                    strip.delete.Enabled = !string.IsNullOrEmpty(SelectedText);
-                    strip.selectAll.Enabled = !string.IsNullOrEmpty(Text);
-                }
-            }
-
-         }
-
-        private class TextBoxContextMenuStrip : MaterialContextMenuStrip
-        {
-            public readonly ToolStripItem undo = new MaterialToolStripMenuItem { Text = "Undo" };
-            public readonly ToolStripItem seperator1 = new ToolStripSeparator();
-            public readonly ToolStripItem cut = new MaterialToolStripMenuItem { Text = "Cut" };
-            public readonly ToolStripItem copy = new MaterialToolStripMenuItem { Text = "Copy" };
-            public readonly ToolStripItem paste = new MaterialToolStripMenuItem { Text = "Paste" };
-            public readonly ToolStripItem delete = new MaterialToolStripMenuItem { Text = "Delete" };
-            public readonly ToolStripItem seperator2 = new ToolStripSeparator();
-            public readonly ToolStripItem selectAll = new MaterialToolStripMenuItem { Text = "Select All" };
-
-            public TextBoxContextMenuStrip()
-            {
-                Items.AddRange(new[]
-                {
-                    undo,
-                    seperator1,
-                    cut,
-                    copy,
-                    paste,
-                    delete,
-                    seperator2,
-                    selectAll
-                });
-            }
-        }
     }
 }
