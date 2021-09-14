2107ba2779a2c5a95a3852f849cb5cb514df7792
orapps44 <77468294+orapps44@users.noreply.github.com>
2021-09-14 22:44:17 +0200

    Update MaterialContextMenuStrip.cs

--- MaterialSkin/Controls/MaterialContextMenuStrip.cs
+++ MaterialSkin/Controls/MaterialContextMenuStrip.cs
@@ -7,6 +7,7 @@
     using System.Drawing.Text;
     using System.Windows.Forms;
 
+    [ToolboxItem(false)]
     public class MaterialContextMenuStrip : ContextMenuStrip, IMaterialControl
     {
         //Properties for managing the material design properties
